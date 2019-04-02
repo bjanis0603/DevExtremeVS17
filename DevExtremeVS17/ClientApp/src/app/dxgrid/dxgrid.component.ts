80d8c5997cfc5058fec45342a6664b55c60b16b9
bjanis0603 <bjanis@practicevelocity.com>
2019-04-02 11:08:18 -0500

    Added sorting and searching

--- DevExtremeVS17/ClientApp/src/app/dxgrid/dxgrid.component.ts
+++ DevExtremeVS17/ClientApp/src/app/dxgrid/dxgrid.component.ts
@@ -13,4 +13,41 @@ export class DxGridComponent {
   constructor(service: Service) {
     this.messages = service.getMessages();
   }
+
+  //When a dxTemplate is used, sorting does not happen automatically; so, we need to specify what to sort on and set allowSorting="true"
+  sortIsUrgentColumn(rowData) {
+    return rowData.urgent == true? 0: 1;
+  }
+
+  sortPatNameColumn(rowData) {
+    return rowData.patNum;
+  }
+
+  sortDateOfVisitColumn(rowData) {
+    return rowData.DateOfVisit;
+  }
+
+  sortProviderNameColumn(rowData) {
+    return rowData.provider;
+  }
+
+  sortMessageColumn(rowData) {
+    return rowData.messageText;
+  }
+
+  calculateCellNameValue(rowData) {
+    return rowData.patNum + ' ' + rowData.patName;
+  }
+
+  calculateCellDateOfVisitValue(rowData) {
+    return rowData.dateOfVisit;
+  }
+
+  calculateCellProviderValue(rowData) {
+    return rowData.provider + ' ' + rowData.providerNum;
+  }
+
+  calculateCellMessageValue(rowData) {
+    return rowData.createdBy + ' ' + rowData.subject + ' ' + rowData.messageText;
+  }
 }
